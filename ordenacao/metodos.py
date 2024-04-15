db385a939832b390e9c8379e6dc7df48c9f9cc94
juliatbarbosa <103785929+juliatbarbosa@users.noreply.github.com>
2024-04-14 23:22:36 -0300

    aula

--- ordenacao/metodos.py
+++ ordenacao/metodos.py
@@ -1,32 +1,31 @@
 def intercala(inicio, meio, fim, lista):
+    # print(f"inicio={inicio}, meio={meio}, fim={fim}")
     w_lista = []
     i = inicio
     j = meio
-    while (i < meio and j < fim):
-        if (lista[i] < lista[j]):
+    while i < meio and j < fim:
+        if lista[i] < lista[j]:
             w_lista.append(lista[i])
-            i+=1
+            i = i + 1
         else:
             w_lista.append(lista[j])
-            j+=1
-
+            j = j + 1
+    
     while j < fim:
-        w_lista.append(lista[j])  
-        j+=1
-
+        w_lista.append(lista[j])
+        j += 1
     while i < meio:
         w_lista.append(lista[i])
-        i+=1
-
+        i += 1
+    
     for k in range(inicio, fim):
         lista[k] = w_lista[k-inicio]
-    
 
 
 def merge_sort(inicio, fim, lista):
-    if inicio < fim - 1:
-        meio = (inicio + fim) // 2
-        merge_sort(inicio, meio - 1, lista)
+    if inicio < fim-1:
+        meio = (inicio+fim)//2
+        merge_sort(inicio, meio, lista)
         merge_sort(meio, fim, lista)
         intercala(inicio, meio, fim, lista)
 
@@ -38,4 +37,5 @@ if __name__ == "__main__":
         valores.append(random.randint(1, 1000))
     tamanho = len(valores)
     merge_sort(0, tamanho, valores)
+    print("ordenado")
     print(valores)
